d3dc20f9323e172eee0e25b5241860b1a6a93b12
William Aldrich <william.aldrich.63@gmail.com>
2026-02-16 12:04:35 -0700

    update dependencies for build

--- src/main/java/com/example/TelemetryJob.java
+++ src/main/java/com/example/TelemetryJob.java
@@ -1,4 +1,22 @@
 package com.example;
 
+import org.apache.flink.contrib.streaming.state.EmbeddedRocksDBStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 public class TelemetryJob {
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env =
+                StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.enableCheckpointing(10000, CheckpointingMode.EXACTLY_ONCE);
+
+        env.setStateBackend(new EmbeddedRocksDBStateBackend());
+
+        // Kafka source and sink will be added here
+        // Transformations placeholder
+
+        env.execute("Vehicle Telemetry Processing Job");
+    }
 }
